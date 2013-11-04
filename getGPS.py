5afbc8995d65dc168dbfbc84c20c5128bada9807
Raemond Bergstrom-Wood <raemond.bw@gmail.com>
2013-11-03 23:02:34 -0800

    fixed the gps coordinant getter.

--- getGPS.py
+++ getGPS.py
@@ -9,17 +9,17 @@ for agency in [item['agency'] for item in agencies]:
     minLon = float('inf')
     maxLon = -float('inf')
     routesPage = urllib2.urlopen("http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a="\
-        + agency)
-    root = ET.fromstring(routesPage).read()
+        + agency).read()
+    root = ET.fromstring(routesPage)
     for routeTag in [route.attrib['tag'] for route in root.iter('route')]:
-        #get the longitude and latitude from the route page to develop and idea of where the transit agencies are
+        # get the longitude and latitude from the route page to develop and idea of where the transit agencies are
         routePage = urllib2.urlopen("http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a="\
-            +agency+"&r="+routeTag)
-        routeRoot = ET.fromstring(routePage).read()
-        latmin = int(routeRoot[0].attrib['latMin'])
-        latmax = int(routeRoot[0].attrib['latMax'])
-        lonmin = int(routeRoot[0].attrib['lonMin'])
-        lonmax = int(routeRoot[0].attrib['lonMax'])
+            +agency+"&r="+routeTag).read()
+        routeRoot = ET.fromstring(routePage)
+        latmin = float(routeRoot[0].attrib['latMin'])
+        latmax = float(routeRoot[0].attrib['latMax'])
+        lonmin = float(routeRoot[0].attrib['lonMin'])
+        lonmax = float(routeRoot[0].attrib['lonMax'])
         if latmin < minLat:
             minLat = latmin
         if latmax > maxLat:
@@ -28,6 +28,7 @@ for agency in [item['agency'] for item in agencies]:
             minLon = lonmin
         if lonmax > maxLon:
             maxLon = lonmax
+        time.sleep(.2)
     print "'minLat':'" +str(minLat) + "','maxLat':'" + str(maxLat) +"','minLon':'"+ str(minLon) +"','maxLon':'"+ str(maxLon)+"'"
     time.sleep(3)
 
