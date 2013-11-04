8fd6a070fed7ecff3f2d1776fa29a750b0794361
Raemond Bergstrom-Wood <raemond.bw@gmail.com>
2013-11-04 10:50:46 -0800

    made a static simple agency list.

--- restserver.py
+++ restserver.py
@@ -1,7 +1,7 @@
 #!flask/bin/python
 from flask import Flask, jsonify, abort, request, make_response, url_for
 #from flask.ext.httpauth import HTTPBasicAuth
-from resources import agencies
+from resources import agencies, agencyList
 
 app = Flask(__name__, static_url_path = "")
 #auth = HTTPBasicAuth()
@@ -10,6 +10,11 @@ app = Flask(__name__, static_url_path = "")
 def returnAgencies():
     return url_for('static', filename="agencies.json")
     #return jsonify({'agencies' : agencies})
+
+@app.route('/agencyList', methods = ['get'])
+def returnAgencyList():
+    return url_for('static', filename="agencyList.json")
+    #return jsonify({'agencyList' : agencyList})
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10080)
